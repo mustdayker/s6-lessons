11c1439d2ea1b3b580d9f263371f599690ab2518
Dmitry Kosarev <mustdayker@gmail.com>
2023-07-19 08:26:38 +0300

    update

--- Тема 2. Аналитические СУБД. Vertica/3. Знакомство с Vertica/Задание 1/realization.py
+++ Тема 2. Аналитические СУБД. Vertica/3. Знакомство с Vertica/Задание 1/realization.py
@@ -1,18 +1,23 @@
 import vertica_python
 
-conn_info = {'host': '# Адрес сервера из инструкции', 
-             'port': '# Порт из инструкции',
-             'user': '# Полученный логин',       
-             'password': '# Пароль',
+conn_info = {'host': 'vertica.tgcloudenv.ru', 
+             'port': '5433',
+             'user': 'stv2023070319',       
+             'password': 'zmb2aHGYQIPkxIS',
              'database': 'dwh',
              # Вначале он нам понадобится, а дальше — решите позже сами
             'autocommit': True
 }
 
 def try_select(conn_info=conn_info):
-	# И рекомендуем использовать соединение вот так
-	with vertica_python.connect(**conn_info) as conn:
-		# Select 1 — ваш код здесь; 
-
-		res = cur.fetchall()
-		return res
+    # И рекомендуем использовать соединение вот так
+    with vertica_python.connect(**conn_info) as conn:
+    
+        # Select 1 — ваш код здесь; 
+        querry = 'SELECT 1 as a1;'
+        
+        cur = conn.cursor()
+        cur.execute(querry) 
+        
+        res = cur.fetchall()
+        return res
